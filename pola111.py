42e6f5f7318c846d166f4045f689fc671f5efcc5
anthonypolanco00001@gmail.com <anthonypolanco00001@gmail.com>
2025-06-23 09:01:02 -0500

    Archivo de owner

--- pola111.py
+++
@@ -1,7 +0,0 @@
-# archivo2.py
-# 2. Calcular el factorial de un número
-n = int(input("Ingrese un número: "))
-factorial = 1
-for i in range(1, n + 1):
-    factorial *= i
-print("El factorial es", factorial)
